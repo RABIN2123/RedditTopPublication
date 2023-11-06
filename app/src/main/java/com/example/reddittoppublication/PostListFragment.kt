6280d269d5e0d967dac44d9a3d52dd98baed160b
RABIN2123 <alexslitvin@gmail.com>
2023-11-06 19:32:44 +0200

    Basic interface layout created

---
+++ app/src/main/java/com/example/reddittoppublication/PostListFragment.kt
@@ -0,0 +1,11 @@
+package com.example.reddittoppublication
+
+import androidx.fragment.app.Fragment
+
+class PostListFragment : Fragment() {
+    companion object {
+        fun newInstance() = PostListFragment.apply {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/reddittoppublication/WindowPictureFragment.kt
@@ -0,0 +1,12 @@
+package com.example.reddittoppublication
+
+import androidx.fragment.app.Fragment
+
+class WindowPictureFragment : Fragment() {
+
+    companion object{
+        fun newInstance() = WindowPictureFragment.apply {
+
+        }
+    }
+}
